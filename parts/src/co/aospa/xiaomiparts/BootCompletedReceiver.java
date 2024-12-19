DigiGoon/android_device_xiaomi_sm8350-common

[SQUASH] sm8350-common: parts: Import DC Dimming and HBM
this squash imports DC Dimming and HBM to xiaomi parts and rootdir
it is adjusted to for sm8350
(*＞ω＜*)♡

inspired by

HBM commit:
- https://github.com/Mudit200408/new_device_xiaomi_munch/commit/87eab66cf179679143cb904b118358ed74246299

DC dimming commits:

- https://github.com/Mudit200408/new_device_xiaomi_munch/commit/6db27e28ef6e01ec092f43b20d072f61c61c1a33
- https://github.com/Mudit200408/new_device_xiaomi_munch/commit/49be770010693b49db1628242b6902ee942c6f42
- https://github.com/Mudit200408/new_device_xiaomi_munch/commit/1e4393d160d5eef8a51e135fa83e621c50a47b13

blazerpaul15's edit:
- Make hbm state writable by system
- Fix HBM path

Co-authored-by: Arian <39061882+ArianK16a@users.noreply.github.com>
Co-authored-by: Nauval Rizky <enuma.alrizky@gmail.com>
Co-authored-by: kamikaonashi <thorschi93@gmx.at>
Co-authored-by: blazerpaul15 <blazerpaul15@gmail.com>
Signed-off-by: DigiGoon <kagatharatarang@gmail.com>

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,22 +20,34 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.refreshrate.RefreshUtils;
+import org.lineageos.settings.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_exposure";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+    
         if (DEBUG)
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.onBootCompleted(context);
         ThermalUtils.startService(context);
         RefreshUtils.initialize(context);
+        FileUtils.enableService(context);
+        
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
     }
 }

parts/src/org/lineageos/settings/hbm/AutoHBMService.java
@@ -1,0 +1,155 @@
+package org.lineageos.settings.hbm;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import androidx.preference.PreferenceManager;
+import android.provider.Settings;
+
+import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.display.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class AutoHBMService extends Service {
+    private static final String HBM = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
+    private static final String BACKLIGHT = "/sys/class/backlight/panel0-backlight/brightness";
+
+    private static boolean mAutoHBMActive = false;
+    private ExecutorService mExecutorService;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
+    private boolean dcDimmingEnabled;
+
+    public void activateLightSensorRead() {
+        submit(() -> {
+            mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+            mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        });
+    }
+
+    public void deactivateLightSensorRead() {
+        submit(() -> {
+            mSensorManager.unregisterListener(mSensorEventListener);
+            mAutoHBMActive = false;
+            enableHBM(false);
+        });
+    }
+
+    private void enableHBM(boolean enable) {
+        if (enable) {
+            FileUtils.writeLine(HBM, "1");
+            FileUtils.writeLine(BACKLIGHT, "2047");
+            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
+        } else {
+            FileUtils.writeLine(HBM, "0");
+        }
+    }
+
+    private boolean isCurrentlyEnabled() {
+        return FileUtils.getFileValueAsBoolean(HBM, false);
+    }
+
+    private SensorEventListener mSensorEventListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            KeyguardManager km =
+                (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
+            boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
+            float luxThreshold = Float.parseFloat(mSharedPrefs.getString(HBMFragment.KEY_AUTO_HBM_THRESHOLD, "20000"));
+            long timeToDisableHBM = Long.parseLong(mSharedPrefs.getString(HBMFragment.KEY_HBM_DISABLE_TIME, "1"));
+
+            if (lux > luxThreshold) {
+                if ((!mAutoHBMActive || !isCurrentlyEnabled()) && !keyguardShowing && !dcDimmingEnabled) {
+                    mAutoHBMActive = true;
+                    enableHBM(true);
+                }
+            }
+            if (lux < luxThreshold) {
+                if (mAutoHBMActive) {
+                    mExecutorService.submit(() -> {
+                        try {
+                            Thread.sleep(timeToDisableHBM * 1000);
+                        } catch (InterruptedException e) {
+                        }
+                        if (lux < luxThreshold) {
+                            mAutoHBMActive = false;
+                            enableHBM(false);
+                        }
+                    });
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        mExecutorService = Executors.newSingleThreadExecutor();
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    private Future < ? > submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

parts/src/org/lineageos/settings/hbm/HBMModeTileService.java
@@ -1,0 +1,100 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package org.lineageos.settings.hbm;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.BroadcastReceiver;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+import android.provider.Settings;
+
+import org.lineageos.settings.utils.FileUtils;
+import org.lineageos.settings.display.*;
+
+public class HBMModeTileService extends TileService {
+
+    private static final String HBM = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
+    private static final String HBM_KEY = "hbm";
+    private static final String BACKLIGHT = "/sys/class/backlight/panel0-backlight/brightness";
+
+    private BroadcastReceiver screenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+                sharedPrefs.edit().putBoolean(HBM_KEY, false).commit();
+                updateUI(false);
+            }
+        }
+    };
+
+    private void updateUI(boolean enabled) {
+        final Tile tile = getQsTile();
+        tile.setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(screenStateReceiver, filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(screenStateReceiver);
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        updateUI(sharedPrefs.getBoolean(HBM_KEY, false));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final boolean dcDimmingEnabled = sharedPrefs.getBoolean(DcDimmingTileService.DC_DIMMING_ENABLE_KEY, false);
+        if (dcDimmingEnabled) {
+            return;
+        }
+        final boolean enabled = !(sharedPrefs.getBoolean(HBM_KEY, false));
+        FileUtils.writeLine(HBM, enabled ? "1" : "0");
+        if (enabled) {
+            FileUtils.writeLine(BACKLIGHT, "2047");
+            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
+        }
+        sharedPrefs.edit().putBoolean(HBM_KEY, enabled).commit();
+        updateUI(enabled);
+    }
+}

parts/src/org/lineageos/settings/utils/FileUtils.java
@@ -16,6 +16,10 @@
 
 package org.lineageos.settings.utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -25,6 +29,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import org.lineageos.settings.hbm.HBMFragment;
+import org.lineageos.settings.hbm.AutoHBMService;
 
 public final class FileUtils {
     private static final String TAG = "FileUtils";
@@ -156,5 +163,43 @@ public final class FileUtils {
             Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
         }
         return ok;
+    }
+    
+    public static boolean getFileValueAsBoolean(String filename, boolean defValue) {
+        String fileValue = readOneLine(filename);
+        if(fileValue!=null){
+            return (fileValue.equals("0")?false:true);
+        }
+        return defValue;
+    }
+
+    public static String getFileValue(String filename, String defValue) {
+        String fileValue = readOneLine(filename);
+        if(fileValue!=null){
+            return fileValue;
+        }
+        return defValue;
+    }
+
+    private static boolean mServiceEnabled = false;
+
+    private static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, AutoHBMService.class),
+                UserHandle.CURRENT);
+        mServiceEnabled = true;
+    }
+
+    private static void stopService(Context context) {
+        mServiceEnabled = false;
+        context.stopServiceAsUser(new Intent(context, AutoHBMService.class),
+                UserHandle.CURRENT);
+    }
+
+    public static void enableService(Context context) {
+        if (HBMFragment.isAUTOHBMEnabled(context) && !mServiceEnabled) {
+            startService(context);
+        } else if (!HBMFragment.isAUTOHBMEnabled(context) && mServiceEnabled) {
+            stopService(context);
+        }
     }
 }

Pengu-Sus/device-xiaomi-sm8350

Co-Authored-By: Arian <39061882+ArianK16a@users.noreply.github.com>
Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>
Co-Authored-By: kamikaonashi <thorschi93@gmx.at>
Co-Authored-By: blazerpaul15 <blazerpaul15@gmail.com>

parts/src/co/aospa/xiaomiparts/BootCompletedReceiver.java
@@ -10,18 +10,26 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
 import android.util.Log;
+import androidx.preference.PreferenceManager;
 
 import co.aospa.xiaomiparts.refreshrate.RefreshUtils;
 import co.aospa.xiaomiparts.thermal.ThermalUtils;
+import co.aospa.xiaomiparts.utils.FileUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final String TAG = "XiaomiParts";
+    private static final String DC_DIMMING_ENABLE_KEY = "dc_dimming_enable";
+    private static final String DC_DIMMING_NODE = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/dimlayer_exposure";
     private static final boolean DEBUG = true;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
         if (!intent.getAction().equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
             return;
         }
@@ -32,5 +40,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Thermal
         ThermalUtils.startService(context);
+
+        // DC Dimming
+        FileUtils.enableService(context);
+        boolean dcDimmingEnabled = sharedPrefs.getBoolean(DC_DIMMING_ENABLE_KEY, false);
+        FileUtils.writeLine(DC_DIMMING_NODE, dcDimmingEnabled ? "1" : "0");
     }
 }

parts/src/co/aospa/xiaomiparts/hbm/AutoHBMService.java
@@ -1,0 +1,155 @@
+package co.aospa.xiaomiparts.hbm;
+
+import android.app.KeyguardManager;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.os.PowerManager;
+import androidx.preference.PreferenceManager;
+import android.provider.Settings;
+
+import co.aospa.xiaomiparts.utils.FileUtils;
+import co.aospa.xiaomiparts.display.*;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class AutoHBMService extends Service {
+    private static final String HBM = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
+    private static final String BACKLIGHT = "/sys/class/backlight/panel0-backlight/brightness";
+
+    private static boolean mAutoHBMActive = false;
+    private ExecutorService mExecutorService;
+
+    private SensorManager mSensorManager;
+    Sensor mLightSensor;
+
+    private SharedPreferences mSharedPrefs;
+    private boolean dcDimmingEnabled;
+
+    public void activateLightSensorRead() {
+        submit(() -> {
+            mSensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+            mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            mSensorManager.registerListener(mSensorEventListener, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        });
+    }
+
+    public void deactivateLightSensorRead() {
+        submit(() -> {
+            mSensorManager.unregisterListener(mSensorEventListener);
+            mAutoHBMActive = false;
+            enableHBM(false);
+        });
+    }
+
+    private void enableHBM(boolean enable) {
+        if (enable) {
+            FileUtils.writeLine(HBM, "1");
+            FileUtils.writeLine(BACKLIGHT, "2047");
+            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
+        } else {
+            FileUtils.writeLine(HBM, "0");
+        }
+    }
+
+    private boolean isCurrentlyEnabled() {
+        return FileUtils.getFileValueAsBoolean(HBM, false);
+    }
+
+    private SensorEventListener mSensorEventListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float lux = event.values[0];
+            KeyguardManager km =
+                (KeyguardManager) getSystemService(getApplicationContext().KEYGUARD_SERVICE);
+            boolean keyguardShowing = km.inKeyguardRestrictedInputMode();
+            float luxThreshold = Float.parseFloat(mSharedPrefs.getString(HBMFragment.KEY_AUTO_HBM_THRESHOLD, "20000"));
+            long timeToDisableHBM = Long.parseLong(mSharedPrefs.getString(HBMFragment.KEY_HBM_DISABLE_TIME, "1"));
+
+            if (lux > luxThreshold) {
+                if ((!mAutoHBMActive || !isCurrentlyEnabled()) && !keyguardShowing && !dcDimmingEnabled) {
+                    mAutoHBMActive = true;
+                    enableHBM(true);
+                }
+            }
+            if (lux < luxThreshold) {
+                if (mAutoHBMActive) {
+                    mExecutorService.submit(() -> {
+                        try {
+                            Thread.sleep(timeToDisableHBM * 1000);
+                        } catch (InterruptedException e) {
+                        }
+                        if (lux < luxThreshold) {
+                            mAutoHBMActive = false;
+                            enableHBM(false);
+                        }
+                    });
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // do nothing
+        }
+    };
+
+    private BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                activateLightSensorRead();
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                deactivateLightSensorRead();
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        mExecutorService = Executors.newSingleThreadExecutor();
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
+        mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            activateLightSensorRead();
+        }
+    }
+
+    private Future < ? > submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mScreenStateReceiver);
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        if (pm.isInteractive()) {
+            deactivateLightSensorRead();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}

parts/src/co/aospa/xiaomiparts/hbm/HBMModeTileService.java
@@ -1,0 +1,100 @@
+/*
+* Copyright (C) 2018 The OmniROM Project
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package co.aospa.xiaomiparts.hbm;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.BroadcastReceiver;
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+import androidx.preference.PreferenceManager;
+import android.provider.Settings;
+
+import co.aospa.xiaomiparts.utils.FileUtils;
+import co.aospa.xiaomiparts.display.*;
+
+public class HBMModeTileService extends TileService {
+
+    private static final String HBM = "/sys/devices/platform/soc/soc:qcom,dsi-display-primary/hbm";
+    private static final String HBM_KEY = "hbm";
+    private static final String BACKLIGHT = "/sys/class/backlight/panel0-backlight/brightness";
+
+    private BroadcastReceiver screenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+                sharedPrefs.edit().putBoolean(HBM_KEY, false).commit();
+                updateUI(false);
+            }
+        }
+    };
+
+    private void updateUI(boolean enabled) {
+        final Tile tile = getQsTile();
+        tile.setState(enabled ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.updateTile();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(screenStateReceiver, filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(screenStateReceiver);
+    }
+
+    @Override
+    public void onStartListening() {
+        super.onStartListening();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        updateUI(sharedPrefs.getBoolean(HBM_KEY, false));
+    }
+
+    @Override
+    public void onStopListening() {
+        super.onStopListening();
+    }
+    @Override
+    public void onClick() {
+        super.onClick();
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final boolean dcDimmingEnabled = sharedPrefs.getBoolean(DcDimmingTileService.DC_DIMMING_ENABLE_KEY, false);
+        if (dcDimmingEnabled) {
+            return;
+        }
+        final boolean enabled = !(sharedPrefs.getBoolean(HBM_KEY, false));
+        FileUtils.writeLine(HBM, enabled ? "1" : "0");
+        if (enabled) {
+            FileUtils.writeLine(BACKLIGHT, "2047");
+            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
+        }
+        sharedPrefs.edit().putBoolean(HBM_KEY, enabled).commit();
+        updateUI(enabled);
+    }
+}

parts/src/co/aospa/xiaomiparts/utils/FileUtils.java
@@ -16,6 +16,10 @@
 
 package co.aospa.xiaomiparts.utils;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -25,6 +29,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import co.aospa.xiaomiparts.hbm.HBMFragment;
+import co.aospa.xiaomiparts.hbm.AutoHBMService;
 
 public final class FileUtils {
     private static final String TAG = "FileUtils";
@@ -156,5 +163,43 @@ public final class FileUtils {
             Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
         }
         return ok;
+    }
+    
+    public static boolean getFileValueAsBoolean(String filename, boolean defValue) {
+        String fileValue = readOneLine(filename);
+        if(fileValue!=null){
+            return (fileValue.equals("0")?false:true);
+        }
+        return defValue;
+    }
+
+    public static String getFileValue(String filename, String defValue) {
+        String fileValue = readOneLine(filename);
+        if(fileValue!=null){
+            return fileValue;
+        }
+        return defValue;
+    }
+
+    private static boolean mServiceEnabled = false;
+
+    private static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, AutoHBMService.class),
+                UserHandle.CURRENT);
+        mServiceEnabled = true;
+    }
+
+    private static void stopService(Context context) {
+        mServiceEnabled = false;
+        context.stopServiceAsUser(new Intent(context, AutoHBMService.class),
+                UserHandle.CURRENT);
+    }
+
+    public static void enableService(Context context) {
+        if (HBMFragment.isAUTOHBMEnabled(context) && !mServiceEnabled) {
+            startService(context);
+        } else if (!HBMFragment.isAUTOHBMEnabled(context) && mServiceEnabled) {
+            stopService(context);
+        }
     }
 }